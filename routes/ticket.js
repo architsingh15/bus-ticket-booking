759f52cb713f20d62e532f0cf604978fbe5f4ed8
architsingh <archit.singh@hashedin.com>
2019-12-17 19:38:42 +0530

    added the user model and updated the create ticket API for creating user and relating it with a ticket as a one to one relationship

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,14 +1,24 @@
+// improve error messaging
 const express = require('express')
 const Ticket = require('../models/Ticket')
+const User = require('../models/User')
 
 const router = express.Router()
 
 //create a ticket
 router.post('/ticket', (req, res) => {
-    const ticket = new Ticket(req.body)
-    ticket.save()
-        .then(data => res.status(200).json(data))
-        .catch(err => res.json({ message: err }))
+    const { passenger } = req.body
+    const ticket = new Ticket({ seat_number: req.body.seat_number })
+    const user = new User(passenger)
+    user.save(function (err, userCreated) {
+        if (err) return res.json({ message: err })
+        if (userCreated) {
+            ticket.passenger = user._id
+            ticket.save()
+                .then(data => res.status(200).json(data))
+                .catch(err => res.json({ message: err }))
+        }
+    })
 })
 
 module.exports = router
